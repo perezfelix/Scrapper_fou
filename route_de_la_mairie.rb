f607df339a9eedf46e3a5679e1cd6a9ab8375d94
Félix Perez <felix2@hotmail.fr>
2018-10-11 11:24:55 +0200

    exo 1 finis

--- route_de_la_mairie.rb
+++ route_de_la_mairie.rb
@@ -2,15 +2,32 @@ require 'rubygems'
 require 'nokogiri' 
 require 'open-uri'
 
-def get_the_email_of_a_townhal_from_its_webpage
-	mail = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
-	mail.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |node|
-	puts node.text
-	end
+def get_the_email_of_a_townhal_from_its_webpage(url)
+  contact = {}
+  page = Nokogiri::HTML(open(url))
+  contact[:name] = page.xpath("/html/body/div/main/section[1]/div/div/div/p[1]/strong[1]/a").text
+  contact[:email] = page.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]").text
+  contact
 end
 
-def get_all_the_urls_of_val_doise_townhalls
-
+def get_all_the_urls_of_val_doise_townhalls(url, page)
+  urls = []
+  page = Nokogiri::HTML(open(url + page))
+  page.xpath("//a[@class = 'lientxt']/@href").each do |town|
+    urls << url + town.text[2..-1]
+  end
+  urls
 end
 
-get_the_email_of_a_townhal_from_its_webpage
+def get_all_the_emails_of_val_doise_townhalls(website_url, page)
+  contacts = []
+  town_urls = get_all_the_urls_of_val_doise_townhalls(website_url, page)
+  town_urls.each do |town_url|
+    p contact = get_the_email_of_a_townhal_from_its_webpage(town_url)
+    contacts << contact
+  end
+  contacts
+end
+
+puts get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/vaureal.html")
+puts get_all_the_emails_of_val_doise_townhalls("http://annuaire-des-mairies.com/", "val-d-oise.html")
